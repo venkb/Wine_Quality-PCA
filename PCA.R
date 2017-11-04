22464f80700d661a01907b3a4396365d1f6014c7
Venkat <venkataraman.bal@gmail.com>
2017-11-04 13:47:51 -0400

    Initial commit

---
+++ PCA.R
@@ -0,0 +1,46 @@
+library(caTools) #to split into test and training data sets
+library(caret) #to build principal components
+library(e1071) #to build svm classifier
+
+#read data
+setwd('C:\\Venkat\\Github\\PCA')
+dataset = read.csv('Wine.csv')
+
+#split dataset into training and test set
+set.seed(123)
+split = sample.split(Y = dataset$Customer_Segment,
+                     SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+#scale continuous features
+training_set[,-14] = scale(training_set[,-14])
+test_set[,-14] = scale(test_set[,-14])
+
+#apply PCA to training and test data sets
+pca = preProcess(x = training_set[,-14],
+                 method = "pca",
+                 pcaComp = 2)
+training_set_pca = predict(pca, training_set)
+pca = preProcess(x = test_set[,-14],
+                 method = "pca",
+                 pcaComp = 2)
+test_set_pca = predict(pca, test_set)
+
+#move the customer_segment to the last 
+training_set_pca = training_set_pca[,c(2,3,1)]
+test_set_pca = test_set_pca[,c(2,3,1)]
+
+#fit classifier using linear SVM classifier
+classifier_svm = svm(formula = Customer_Segment ~ .,
+                     data = training_set_pca,
+                     type = 'C-classification',
+                     kernel = 'linear')
+
+#predict the test data using linear svm classifier
+y_pred = predict(classifier_svm,
+                 newdata = test_set_pca[,-3])
+
+#build confusion matrix
+cm = table(test_set_pca[,3],y_pred)
+cm
